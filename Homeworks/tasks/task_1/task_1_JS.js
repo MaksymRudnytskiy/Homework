33e5ba92d409da4cf0916bba8a5fb1041310a135
MaksymRudnytskiy <79153071+MaksymRudnytskiy@users.noreply.github.com>
2021-03-24 00:35:30 +0200

    task_1 first exercise

---
+++ Homeworks/tasks/task_1/task_1_JS.js
@@ -0,0 +1,58 @@
+//- присвоить каждому из следующих значений свою переменную: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false Вывести каждую при помощи console.log , alert, document.write
+
+let greeting = 'hello';
+let company = 'owu';
+let domain = 'com';
+let country = 'ua';
+let first = 1;
+let students = 10;
+let negative = -999;
+let years = 3.14;
+let golenYears = 2.7;
+let subsidiaries = 16;
+let active = true;
+let failures = false
+
+console.log(greeting)
+console.log(company)
+console.log(domain)
+console.log(country)
+console.log(first)
+console.log(students)
+console.log(negative)
+console.log(years)
+console.log(golenYears)
+console.log(subsidiaries)
+console.log(active)
+console.log(failures)
+
+alert(greeting)
+alert(company)
+alert(domain)
+alert(country)
+alert(first)
+alert(students)
+alert(negative)
+alert(years)
+alert(golenYears)
+alert(subsidiaries)
+alert(active)
+alert(failures)
+
+document.write(greeting)
+document.write(company)
+document.write(domain)
+document.write(country)
+document.write(first)
+document.write(students)
+document.write(negative)
+document.write(years)
+document.write(golenYears)
+document.write(subsidiaries)
+document.write(active)
+document.write(failures)
+
+
+
+
+
