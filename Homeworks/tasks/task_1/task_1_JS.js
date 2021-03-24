6c37bf7cf28ca18a9b1e9348784b60cd79842087
MaksymRudnytskiy <79153071+MaksymRudnytskiy@users.noreply.github.com>
2021-03-24 17:01:35 +0200

    task_1 part 2

--- Homeworks/tasks/task_1/task_1_JS.js
+++ Homeworks/tasks/task_1/task_1_JS.js
@@ -1,4 +1,5 @@
-//- присвоить каждому из следующих значений свою переменную: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false Вывести каждую при помощи console.log , alert, document.write
+//- Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//   Вивести кожну змінну за допомогою: console.log , alert, document.write
 
 let greeting = 'hello';
 let company = 'owu';
@@ -11,46 +12,153 @@ let years = 3.14;
 let golenYears = 2.7;
 let subsidiaries = 16;
 let active = true;
-let failures = false
+let failures = false;
 
-console.log(greeting)
-console.log(company)
-console.log(domain)
-console.log(country)
-console.log(first)
-console.log(students)
-console.log(negative)
-console.log(years)
-console.log(golenYears)
-console.log(subsidiaries)
-console.log(active)
-console.log(failures)
+console.log(greeting);
+console.log(company);
+console.log(domain);
+console.log(country);
+console.log(first);
+console.log(students);
+console.log(negative);
+console.log(years);
+console.log(golenYears);
+console.log(subsidiaries);
+console.log(active);
+console.log(failures);
 
-alert(greeting)
-alert(company)
-alert(domain)
-alert(country)
-alert(first)
-alert(students)
-alert(negative)
-alert(years)
-alert(golenYears)
-alert(subsidiaries)
-alert(active)
-alert(failures)
+alert(greeting);
+alert(company);
+alert(domain);
+alert(country);
+alert(first);
+alert(students);
+alert(negative);
+alert(years);
+alert(golenYears);
+alert(subsidiaries);
+alert(active);
+alert(failures);
 
-document.write(greeting)
-document.write(company)
-document.write(domain)
-document.write(country)
-document.write(first)
-document.write(students)
-document.write(negative)
-document.write(years)
-document.write(golenYears)
-document.write(subsidiaries)
-document.write(active)
-document.write(failures)
+document.write(greeting);
+document.write(company);
+document.write(domain);
+document.write(country);
+document.write(first);
+document.write(students);
+document.write(negative);
+document.write(years);
+document.write(golenYears);
+document.write(subsidiaries);
+document.write(active);
+document.write(failures);
+
+//- Переприсвоїти кожну змінну з завдання вище на будь які довільні значення.
+//    Вивести кожну змінну за допомогою: console.log , alert, document.write
+greeting = 'Salutations';
+company = 'Okten';
+domain = '.com';
+country = 'Ucraine';
+first = 2;
+students = 35;
+negative = -5;
+years = 5.5;
+golenYears = 4.1;
+subsidiaries = 1;
+active = false;
+failures = true;
+
+console.log(greeting);
+console.log(company);
+console.log(domain);
+console.log(country);
+console.log(first);
+console.log(students);
+console.log(negative);
+console.log(years);
+console.log(golenYears);
+console.log(subsidiaries);
+console.log(active);
+console.log(failures);
+
+alert(greeting);
+alert(company);
+alert(domain);
+alert(country);
+alert(first);
+alert(students);
+alert(negative);
+alert(years);
+alert(golenYears);
+alert(subsidiaries);
+alert(active);
+alert(failures);
+
+document.write(greeting);
+document.write(company);
+document.write(domain);
+document.write(country);
+document.write(first);
+document.write(students);
+document.write(negative);
+document.write(years);
+document.write(golenYears);
+document.write(subsidiaries);
+document.write(active);
+document.write(failures);
+
+//- За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+//    Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
