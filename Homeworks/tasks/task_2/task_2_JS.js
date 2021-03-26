a913ac8eab540a8aa30ef7d573bbef6f0798c5a2
MaksymRudnytskiy <79153071+MaksymRudnytskiy@users.noreply.github.com>
2021-03-26 13:22:57 +0200

    task_2 part 1

---
+++ Homeworks/tasks/task_2/task_2_JS.js
@@ -0,0 +1,48 @@
+//--створити масив та вивести його в консоль:
+//- з 5 числових значень
+//- з 5 стічкових значень
+//- з 5 значень стрічкового, числового та булевого типу
+
+let block1 = [3, 7, 11, 87, 998];
+console.log(block1);
+
+let block2 = ['Max', 'Taras', 'laptop', 'car', 'Oksana'];
+console.log(block2);
+
+const block3 = ['computer', 'chair', 56, true, 9];
+console.log(block3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
