f511d8665369585fa239435e189ed56c96bbf32e
MaksymRudnytskiy <79153071+MaksymRudnytskiy@users.noreply.github.com>
2021-03-30 16:42:45 +0300

    task_3 part 3

--- Homeworks/tasks/task_3/task_3_JS.js
+++ Homeworks/tasks/task_3/task_3_JS.js
@@ -45,12 +45,89 @@
 //console.log(idRules);
 //
 ////h) отримати всі елементи з класом fc_rules
+//
+//let h = document.getElementsByClassName('fc_rules');
+//console.log(h);
+//
+//////i) поміняти колір тексту у всіх елементів fc_rules на червони
+//
+//for (let i = 0; i < h.length; i++) {
+//    h[i].style.color = 'red';
+//}
+//
+//2) За допомоги циклу проітерувати  масив users, записати кожного юзера в сівй блок за допомоги document.createElement.
+//    Вставити цей блок на сторінку
+//3) За допомоги циклу проітерувати  масив users, записати кожного юзера в сівй блок за допомоги document.createElement.
+//    Блок з адресою зробити окремим блоком, з блоками для кожної властивості
 
-let h = document.getElementsByClassName('fc_rules');
-console.log(h);
+let users = [{
+    name: 'vasya',
+    age: 31,
+    status: false,
+    address: {city: 'Lviv', country: 'Ukraine', street: 'Shevchenko', houseNumber: 1}
+}, {
+    name: 'petya',
+    age: 30,
+    status: true,
+    address: {city: 'New York', country: 'USA', street: 'East str', houseNumber: 21}
+}, {
+    name: 'kolya',
+    age: 29,
+    status: true,
+    address: {city: 'Budapest', country: 'Hungary', street: 'Kuraku', houseNumber: 78}
+}, {
+    name: 'olya',
+    age: 28,
+    status: false,
+    address: {city: 'Prague', country: 'Czech', street: 'Paster', houseNumber: 56}
+}, {
+    name: 'max',
+    age: 30,
+    status: true,
+    address: {city: 'Istanbul', country: 'Turkey', street: 'Mikar', houseNumber: 39}
+}, {
+    name: 'anya',
+    age: 31,
+    status: false,
+    address: {city: 'Rio', country: 'Brasil', street: 'Ronaldi', houseNumber: 5}
+}, {
+    name: 'oleg',
+    age: 28,
+    status: false,
+    address: {city: 'Montreal', country: 'Canada', street: 'Acusto', houseNumber: 90}
+}, {
+    name: 'andrey',
+    age: 29,
+    status: true,
+    address: {city: 'Quebeck', country: 'Canada', street: 'Binaro', houseNumber: 33}
+}, {
+    name: 'masha',
+    age: 30,
+    status: true,
+    address: {city: 'Moscow', country: 'Russia', street: 'Gogolia', houseNumber: 1}
+}, {
+    name: 'olya',
+    age: 31,
+    status: false,
+    address: {city: 'Portland', country: 'USA', street: 'Forest str', houseNumber: 4}
+}, {
+    name: 'max',
+    age: 31,
+    status: true,
+    address: {city: 'Cairo', country: 'Egypt', street: 'Seashore', houseNumber: 45}
+}];
 
-////i) поміняти колір тексту у всіх елементів fc_rules на червони
+//2) За допомоги циклу проітерувати  масив users, записати кожного юзера в сівй блок за допомоги document.createElement.
+//    Вставити цей блок на сторінку
 
-for (let i = 0; i < h.length; i++) {
-    h[i].style.color = 'red';
+for (let i = 0; i < users.length; i++) {
+    let userDiv = document.createElement('div');
+    userDiv.innerHTML = `name: ${users[i].name}, age: ${users[i].age}, status: ${users[i].status}}`
+    document.body.appendChild(userDiv)
 }
+
+//3) За допомоги циклу проітерувати  масив users, записати кожного юзера в сівй блок за допомоги document.createElement.
+//    Блок з адресою зробити окремим блоком, з блоками для кожної властивості
+
+
+
