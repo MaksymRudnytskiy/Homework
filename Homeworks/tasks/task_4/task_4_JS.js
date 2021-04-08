549497d4ae2fd66f41b9d9387b562fed3dff462e
MaksymRudnytskiy <79153071+MaksymRudnytskiy@users.noreply.github.com>
2021-04-08 17:47:56 +0300

    task_4 part 4

--- Homeworks/tasks/task_4/task_4_JS.js
+++ Homeworks/tasks/task_4/task_4_JS.js
@@ -9,7 +9,7 @@
 //
 //arrFunction(arr);
 //arrFunction();
-//
+
 //2) створити функцію яка заповнює масив рандомними числами та виводить його. Для виведення використати попередню функцію.
 //
 //let arr = [1, 65, 'Max', true, 345];
@@ -23,7 +23,7 @@
 //}
 //
 //arrFunction(arr);
-//
+
 //3) створити функцію яка приймає три числа та виводить найменьше. (Без Math.min!)
 //
 //function returnmin(a,b,c) {
@@ -35,8 +35,7 @@
 //}
 //let min = returnmin(5,6,7);
 //console.log(min);
-//
-//
+
 ////4) створити функцію яка приймає три числа та виводить найбільше. (Без Math.max!)
 //
 //function returnmax(a,b,c) {
@@ -48,7 +47,7 @@
 //}
 //let max = returnmax(5,6,7);
 //console.log(max);
-//
+
 //5) створити функцію яка повертає найбільше число з масиву
 //
 //function returnmax(a,b,c) {
@@ -65,7 +64,7 @@
 //const maxReturn = arr.push(max)
 //
 //console.log(arr);
-//
+
 //6) створити функцію яка повертає найменьше число з масиву
 //
 //function returnmin(a,b,c) {
@@ -82,26 +81,80 @@
 //const minReturn = arr2.push(min)
 //
 //console.log(arr2);
-//
+
 //7) створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його.
-
-
+//
+//const arrNum = [33, 23, 12, 543, 219];
+//
+//function returnArr() {
+//    let sum = 0;
+//    for (let i = 0; i < arrNum.length; i++) {
+//        sum += arrNum[i];
+//
+//    }
+//    return sum;
+//}
+//
+//let sum = returnArr()
+//
+//console.log(sum);
 
 //8) створити функцію яка приймає масив чисел та повертає середнє арифметичне його значень.
-
-
+//
+//const arrNum2 = [33, 23, 12, 543, 219];
+//
+//function returnArr2() {
+//    let sum = 0;
+//    for (let i = 0; i < arrNum2.length; i++) {
+//        sum += arrNum2[i];
+//    }
+//    let arrNum2Content = arrNum2.length;
+//    let average = sum / arrNum2Content;
+//
+//    return average;
+//}
+//
+//let sum2 = returnArr2()
+//
+//console.log(sum2);
 
 //9) Створити функцію яка приймає масив будь яких объектів, та повертає масив ключів всіх обєктів
 //EXAMPLE:
 //    [{name: 'Dima', age: 13}, {model: 'Camry'}]  ===> [ name, age, model ]
-
-
+//
+//const mixArr = [{name: 'Dima', age: 13}, {model: 'Camry'}]
+//
+//function arrKey() {
+//    const arr2 = [];
+//    for (let item of mixArr) {
+//        for (let key in item) {
+//            arr2.push(key);
+//        }
+//    }
+//    return arr2;
+//}
+//
+//let arr = arrKey()
+//console.log(arr)
 
 //10) Створити функцію яка приймає масив будь яких объектів, та повертає масив значень всіх обєктів
 //EXAMPLE:
 //    [{name: 'Dima', age: 13}, {model: 'Camry'}]  ===> [ Dima, 13, Camry ]
-
-
+//
+//const mixArr = [{name: 'Dima', age: 13}, {model: 'Camry'}]
+//
+//function arrValue() {
+//    const arr2 = [];
+//    for (let item of mixArr) {
+//        for (let values in item) {
+//            arr2.push(values);
+//        }
+//    }
+//    return arr2;
+//}
+//
+//let arr = arrValue()
+//console.log(arr)
 
 //11) створити функцію  яка скаладає значення елементів з однаковими індексами  та повертає новий результуючий масив.
 //    EXAMPLE:
@@ -110,14 +163,23 @@
 //результат
 //    [3,5,7,9]
 
+const arr1 = [1,2,3,4];
 
+const arr2 = [2,3,4,5];
 
+function sumArr () {
+    let sumArr3 = 0
+    for (let i = 0; i < arr1.length; i++) {
+        for (let l = 0; l < arr2.length; l++) {
+        sumArr3 += arr1[i] + arr2[l];
+        }
+    }
+    return sumArr3
+}
+let sumArr3 = sumArr()
 
+const arr3 = []
 
+const pushArr3 = arr3.push(sumArr3)
 
-
-
-
-
-
-
+console.log(arr3);
